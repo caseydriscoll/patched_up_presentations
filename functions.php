a7a7e33f2213a1104178d8fc19936e5b8f305242
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2014-11-02 19:12:57 +0000

    Adds presentations cpt and url fix

--- functions.php
+++ functions.php
@@ -31,3 +31,76 @@ function shortcode_play( $atts ){
   return "<a id='play'>Toggle Notes</a>";
 }
 add_shortcode( 'play', 'shortcode_play' );
+
+
+function create_presentation_post_type() {
+  register_post_type( 'presentation',
+    array(
+      'labels' => array(
+        'name' => __( 'Presentations' ),
+        'singular_name' => __( 'Presentation' )
+      ),
+      'menu_icon' => '',
+      'public' => true,
+      'has_archive' => true,
+    )
+  );
+}
+add_action( 'init', 'create_presentation_post_type' );
+
+
+function add_menu_icons_styles(){
+?>
+ 
+<style>
+#adminmenu .menu-icon-presentation div.wp-menu-image:before {
+  content: '\f183';
+}
+</style>
+ 
+<?php
+}
+add_action( 'admin_head', 'add_menu_icons_styles' );
+
+
+
+
+// Thanks to http://colorlabsproject.com/tutorials/remove-slugs-custom-post-type-url/
+/**
+ * Remove the slug from published post permalinks.
+ */
+function custom_remove_cpt_slug( $post_link, $post, $leavename ) {
+ 
+    if ( 'presentation' != $post->post_type || 'publish' != $post->post_status ) {
+        return $post_link;
+    }
+ 
+    $post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
+ 
+    return $post_link;
+}
+add_filter( 'post_type_link', 'custom_remove_cpt_slug', 10, 3 );
+
+
+/**
+ * Some hackery to have WordPress match postname to any of our public post types
+ * All of our public post types can have /post-name/ as the slug, so they better be unique across all posts
+ * Typically core only accounts for posts and pages where the slug is /post-name/
+ */
+function custom_parse_request_tricksy( $query ) {
+ 
+    // Only noop the main query
+    if ( ! $query->is_main_query() )
+        return;
+ 
+    // Only noop our very specific rewrite rule match
+    if ( 2 != count( $query->query ) || ! isset( $query->query['page'] ) ) {
+        return;
+    }
+ 
+    // 'name' will be set if post permalinks are just post_name, otherwise the page rule will match
+    if ( ! empty( $query->query['name'] ) ) {
+        $query->set( 'post_type', array( 'post', 'presentation', 'page' ) );
+    }
+}
+add_action( 'pre_get_posts', 'custom_parse_request_tricksy' );
